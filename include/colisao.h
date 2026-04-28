d74455489678bc0e158092992a9ebacabb36a814
José Milton <miltoneto4231@gmail.com>
2026-04-28 20:45:50 -0300

    criação do colisao.h e suas structs

--- include/colisao.h
+++ include/colisao.h
@@ -1,9 +1,21 @@
+//Criação da primeira parte da biblioteca própria de colisão
+
 #ifndef NOME_DO_ARQUIVO_H
 #define NOME_DO_ARQUIVO_H
 
-typedef struct {
-    
-}
+#include "raylib.h" // Aqui incluimos a biblioteca "Raylib, importada do github"
+
+typedef struct Retangulo { // Esta struct cria os valores de X, Y, Altura e Largura para serem usadar na criação de paredes e chão
+    float x;
+    float y;
+    float altura;
+    float largura;
+} Retangulo;
+
+typedef struct colidivel { // Esta struct cria a possibilidade dos blocos serem colididos
+    Retangulo hitbox;
+    int tipo;
+}colidivel;
 
 
 #endif
