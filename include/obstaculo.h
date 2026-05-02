33ce7aac1d663d49e607c5e98996640381168e9e
José Milton <miltoneto4231@gmail.com>
2026-05-02 13:35:35 -0300

    Atualização do obstaculo.h

--- include/obstaculo.h
+++ include/obstaculo.h
@@ -3,9 +3,11 @@
 #include "colisao.h"
 #include "raylib.h"
 
+//Criamos a struct que vai determinar a geração de obstáculos na tela
 typedef struct Obstaculo{ 
     Colidivel hitbox;
     Obstaculo *proximo;
+    float velocidadeX;
 } Obstaculo;
 
 Obstaculo *criar_obstaculo (float x, float y, float altura, float largura, int tipo); 
@@ -13,5 +15,6 @@ void apagar_obstaculo (Obstaculo*);
 Obstaculo *adicionar_obstaculo (Obstaculo* lista, Obstaculo* obstaculo); 
 Obstaculo *remover_obstaculo (Obstaculo* lista, Obstaculo* obstaculo); 
 void sprite_obstaculo (const Obstaculo*); 
-void atualizar_obstaculos (Obstaculo*);   
+//Esta função vai determinar como os obstáculos vão se comportar durante o jogo
+Obstaculo* atualizar_obstaculos(Obstaculo*);   
 #endif
