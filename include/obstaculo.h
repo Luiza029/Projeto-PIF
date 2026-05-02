210a1e108e6fc958b2aa92c77a1f7de8c8e117d3
José Milton <miltoneto4231@gmail.com>
2026-05-02 16:43:03 -0300

    Atualização da obstaculo.h e criando obstaculo.c

--- include/obstaculo.h
+++ include/obstaculo.h
@@ -8,13 +8,14 @@ typedef struct Obstaculo{
     Colidivel hitbox;
     Obstaculo *proximo;
     float velocidadeX;
+    Color cor;
 } Obstaculo;
 
 Obstaculo *criar_obstaculo (float x, float y, float altura, float largura, int tipo); 
-void apagar_obstaculo (Obstaculo*); 
+void apagar_obstaculo (Obstaculo* obs); 
 Obstaculo *adicionar_obstaculo (Obstaculo* lista, Obstaculo* obstaculo); 
 Obstaculo *remover_obstaculo (Obstaculo* lista, Obstaculo* obstaculo); 
-void sprite_obstaculo (const Obstaculo*); 
+void sprite_obstaculo (const Obstaculo* obs); 
 //Esta função vai determinar como os obstáculos vão se comportar durante o jogo
-Obstaculo* atualizar_obstaculos(Obstaculo*);   
+Obstaculo* atualizar_obstaculos(Obstaculo* lista, float delta);   
 #endif
